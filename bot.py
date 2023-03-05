6f1378357c9a2039299e7b3c0cfcab3d7d65e29c
soheil khaledabadi <soheilkhaledabdi@gmail.com>
2023-03-05 20:08:19 +0330

    remove code test

--- bot.py
+++ bot.py
@@ -12,27 +12,6 @@ bot = Client("monitoring server bot"
 
 
 
-start_message = "heya , ali khar kosde"
-start_message_button=[
-    [InlineKeyboardButton('channel' , url="www.codeil.ir"),
-     InlineKeyboardButton('channel' , url="www.codeil.ir")
-     ],
-    [InlineKeyboardButton('channel' , url="www.codeil.ir")]
-]
-
-
-@bot.on_message(filters.command('start') & filters.private)
-def command1(bot,message):
-    text = start_message
-    reply_markup = InlineKeyboardMarkup(start_message_button)
-    message.reply(
-        text=text,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True
-    )
-
-
-
 
 print("bot started")
 bot.run()
